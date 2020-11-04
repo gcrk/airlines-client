f4bfdf73dacc34491fb6cf6d35ad6a664a295c5a
YOUR_FULL NAME_HERE <your@email.here.com>
2020-11-04 11:55:20 +1100

    Seat component added

--- src/components/Airline.js
+++ src/components/Airline.js
@@ -1,10 +1,12 @@
-import React, { Component } from 'react';
+import React, { Component } from 'react'
 import FlightSearch from './FlightSearch'
+import Seat from './Seat'
 class Airline extends Component {
   render() {
     return(
       <div className="form-container">
        <FlightSearch />
+       <Seat />
       </div>
     )
   }
