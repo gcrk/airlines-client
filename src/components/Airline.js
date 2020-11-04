e0932e65f593a3e477c0694d691f92bbcad95e79
YOUR_FULL NAME_HERE <your@email.here.com>
2020-11-04 11:40:34 +1100

    Seat component added

--- src/components/Airline.js
+++ src/components/Airline.js
@@ -1,9 +1,13 @@
 import React , {Component}from 'react';
 import FlightSearch from './FlightSearch'
+import Seat from './Seat'
 class Airline extends Component {
   render(){
     return(
-  <FlightSearch />
+      <div>
+       <FlightSearch />
+       <Seat />
+      </div> 
     )
   }
 
---
+++ src/components/Seat.js
@@ -0,0 +1,11 @@
+import React , {Component}from 'react';
+class Seat extends Component {
+  render(){
+    return(
+     <h4>List of available seats</h4>
+    )
+  }
+
+}
+
+export default Seat;
