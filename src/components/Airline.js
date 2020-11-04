4d072788f3defa6fa1b0a722f9c2b85008f21e68
YOUR_FULL NAME_HERE <your@email.here.com>
2020-11-04 11:03:04 +1100

    first commit

--- src/components/Airline.js
+++ src/components/Airline.js
@@ -1,10 +1,12 @@
-import React from 'react';
-
+import React , {Component}from 'react';
+import FlightSearch from './FlightSearch'
 class Airline extends Component {
-  render()
-  return(
-    <FlightSearch />
-  )
+  render(){
+    return(
+  <FlightSearch />
+    )
+  }
+
 }
 
 export default Airline;
